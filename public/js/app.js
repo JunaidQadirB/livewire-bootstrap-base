9a3e15ba7d6937884ab1bdd7869ce831f6ea28ec
Junaid Qadir Shekhanzai <shekhanzai.baloch@gmail.com>
2020-04-24 22:35:37 +0500

    Clean-up

--- public/js/app.js
+++ public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/timbercasts/Desktop/todid-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/timbercasts/Desktop/todid-app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/livewire-base-app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/livewire-base-app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
-    <title>{{ config('app.name', 'ToDid') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com"/>
